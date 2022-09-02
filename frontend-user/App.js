904359e83a3cce9a2cdb1a2ffc8b117a7f23815d
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-02 11:21:35 +0300

    feat - render restaurant image in card component

--- frontend-user/App.js
+++ frontend-user/App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Restaurant item */}
+      <View style={styles.restaurantContainer}>
+        <Image
+          source={require("./assets/images/restaurants/1.jpeg")}
+          style={styles.image}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  restaurantContainer: {
+    width: "100%",
+  },
+  image: {
+    width: "100%",
+    height: 300,
   },
 });
