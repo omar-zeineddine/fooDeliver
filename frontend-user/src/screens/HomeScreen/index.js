7d82287130af7102a753890856b93c65f9b3ee54
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-02 22:58:30 +0300

    refactor - fix components paths after directory changes

--- frontend-user/src/screens/HomeScreen/index.js
+++ frontend-user/src/screens/HomeScreen/index.js
@@ -1,6 +1,6 @@
 import { StyleSheet, FlatList } from "react-native";
-import RestaurantItem from "./src/components/RestaurantItem";
-import restaurants from "./assets/data/restaurants.json";
+import RestaurantItem from "../../../src/components/RestaurantItem";
+import restaurants from "../../../assets/data/restaurants.json";
 
 const HomeScreen = () => {
   return (
