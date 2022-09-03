bde4437f8fe274af96f26052538376ab0811330c
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-03 11:08:58 +0300

    feat - add restaurant info screen

--- frontend-user/App.js
+++ frontend-user/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
+import RestaurantDetailsScreen from "./src/screens/RestaurantDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <RestaurantDetailsScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ frontend-user/src/screens/RestaurantDetailsScreen/index.js
@@ -0,0 +1,13 @@
+import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+
+const RestaurantDetailsPage = () => {
+  return (
+    <View>
+      <Text>restaurant info</Text>
+    </View>
+  );
+};
+
+export default RestaurantDetailsPage;
+
+const styles = StyleSheet.create({});
