3a2994338eeb9a3593d56428b498653ba7187ace
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-03 13:36:14 +0300

    style - add style for menu items

--- frontend-user/src/components/DishListItem/index.js
+++ frontend-user/src/components/DishListItem/index.js
@@ -1,13 +1,34 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 
-const DishListItem = () => {
+const DishListItem = ({ dish }) => {
   return (
-    <View>
-      <Text>Title</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>{dish.name}</Text>
+      <Text style={styles.description}>{dish.description}</Text>
+      <Text style={styles.price}>$ {dish.price}</Text>
     </View>
   );
 };
 
 export default DishListItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    paddingBottom: 10,
+    borderBottomColor: "lightgrey",
+    borderBottomWidth: 1.5,
+  },
+  name: {
+    fontWeight: "600",
+    fontSize: 17,
+    letterSpacing: 0.5,
+  },
+  description: {
+    color: "grey",
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 16,
+  },
+});
