048b681b63d85836966921b5bdcea158bccf63e0
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-03 11:30:06 +0300

    style - add styles for single restaurant page

--- frontend-user/src/screens/RestaurantDetailsScreen/index.js
+++ frontend-user/src/screens/RestaurantDetailsScreen/index.js
@@ -1,13 +1,47 @@
 import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+import restaurants from "../../../assets/data/restaurants.json";
+
+const restaurant = restaurants[0];
 
 const RestaurantDetailsPage = () => {
   return (
-    <View>
-      <Text>restaurant info</Text>
+    <View style={styles.page}>
+      <Image
+        source={{ uri: restaurant.image }}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <View style={styles.container}>
+        <Text style={styles.title}>{restaurant.name}</Text>
+        <Text style={styles.subtitle}>
+          $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
+          {restaurant.maxDeliveryTime} minutes
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default RestaurantDetailsPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  container: {
+    margin: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5 / 3,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "600",
+  },
+  subtitle: {
+    color: "grey",
+    fontSize: 15,
+    fontWeight: "500",
+  },
+});
