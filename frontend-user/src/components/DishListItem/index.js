78dc528ab31f7772f3ea542e2ff67a049cfe9ad5
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-03 22:58:26 +0300

    feat - add conditional image render to dish item

--- frontend-user/src/components/DishListItem/index.js
+++ frontend-user/src/components/DishListItem/index.js
@@ -3,9 +3,19 @@ import { View, Text, StyleSheet, Image } from "react-native";
 const DishListItem = ({ dish }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{dish.name}</Text>
-      <Text style={styles.description}>{dish.description}</Text>
-      <Text style={styles.price}>$ {dish.price}</Text>
+      <View style={styles.dish}>
+        <Text style={styles.name}>{dish.name}</Text>
+        <Text style={styles.description} numberOfLines={2}>
+          {dish.description}
+        </Text>
+        <Text style={styles.price}>$ {dish.price}</Text>
+      </View>
+      <View>
+        {/* conditional render of image */}
+        {dish.image && (
+          <Image source={{ uri: dish.image }} style={styles.image} />
+        )}
+      </View>
     </View>
   );
 };
@@ -14,14 +24,21 @@ export default DishListItem;
 
 const styles = StyleSheet.create({
   container: {
-    margin: 10,
+    // margin: 10,
+    paddingVertical: 10,
+    marginVertical: 10,
+    marginHorizontal: 20,
     paddingBottom: 10,
     borderBottomColor: "lightgrey",
-    borderBottomWidth: 1.5,
+    borderBottomWidth: 1,
+    flexDirection: "row",
+  },
+  dish: {
+    flex: 1,
   },
   name: {
     fontWeight: "600",
-    fontSize: 17,
+    fontSize: 16,
     letterSpacing: 0.5,
   },
   description: {
@@ -30,5 +47,9 @@ const styles = StyleSheet.create({
   },
   price: {
     fontSize: 16,
+  },
+  image: {
+    height: 80,
+    aspectRatio: 1,
   },
 });
