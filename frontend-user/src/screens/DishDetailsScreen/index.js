6b9ad7e93fd6498f911b70294d132243880607a7
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-04 22:25:29 +0300

    feat - add item quantity counter

--- frontend-user/src/screens/DishDetailsScreen/index.js
+++ frontend-user/src/screens/DishDetailsScreen/index.js
@@ -1,13 +1,54 @@
 import { View, Text, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import restaurants from "../../../assets/data/restaurants.json";
+
+const dish = restaurants[0].dishes[0];
 
 const DishDetailsScreen = () => {
   return (
-    <View>
-      <Text>Dish Details</Text>
+    <View style={styles.page}>
+      <Text style={styles.name}>{dish.name}</Text>
+      <Text style={styles.description}>{dish.description}</Text>
+      <View style={styles.separator}></View>
+      <View style={styles.row}>
+        <AntDesign name="minuscircleo" size={60} color={"black"} />
+        <Text style={styles.quantity}>1</Text>
+        <AntDesign name="pluscircleo" size={60} color={"black"} />
+      </View>
     </View>
   );
 };
 
 export default DishDetailsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    width: "100%",
+    paddingVertical: 30,
+    padding: 10,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  description: {
+    color: "grey",
+  },
+  separator: {
+    backgroundColor: "lightgrey",
+    height: 1,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+  },
+  quantity: {
+    fontSize: 25,
+    marginHorizontal: 20,
+    fontWeight: "bold",
+  },
+});
