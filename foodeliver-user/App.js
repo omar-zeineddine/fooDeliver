4ec6fe2bb59cb14de8021772c21fd25095d24d11
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-08 15:11:29 +0300

    backend - implement authentication

--- foodeliver-user/App.js
+++ foodeliver-user/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigation";
-import { Amplify } from 'aws-amplify'
-import awsconfig from './src/aws-exports'
-Amplify.configure(awsconfig)
 
-export default function App() {
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
+import awsconfig from "./src/aws-exports";
+Amplify.configure(awsconfig);
+
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
@@ -13,3 +15,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App);
