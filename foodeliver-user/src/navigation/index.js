1258bf0aa4fa2624cb9481ccf3ee460c49a7ddd0
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-08 16:22:29 +0300

    feat - add profile screen with user auth

--- foodeliver-user/src/navigation/index.js
+++ foodeliver-user/src/navigation/index.js
@@ -6,6 +6,7 @@ import DishDetailsScreen from "../screens/DishDetailsScreen";
 import BasketScreen from "../screens/BasketScreen";
 import OrdersScreen from "../screens/OrdersScreen";
 import OrderDetails from "../screens/OrderDetails";
+import ProfileScreen from "../screens/ProfileScreen";
 
 import { Foundation, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
@@ -44,7 +45,7 @@ const HomeTabs = () => {
       />
       <Tab.Screen
         name="Profile"
-        component={OrdersScreen}
+        component={ProfileScreen}
         options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome5 name="user-alt" size={24} color={color} />
