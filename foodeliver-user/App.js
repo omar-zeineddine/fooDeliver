e294106aec429a867cee90f8b6935f374cdd09a1
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-08 14:54:19 +0300

    backend - pull env staging from cloud

--- foodeliver-user/App.js
+++ foodeliver-user/App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigation";
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
 
 export default function App() {
   return (
