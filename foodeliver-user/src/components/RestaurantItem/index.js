9cfb6c82f44f8af9e7ba72e7a0ae373a44cc9a66
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-08 19:41:01 +0300

    fix - set float limits to price and ratings

--- foodeliver-user/src/components/RestaurantItem/index.js
+++ foodeliver-user/src/components/RestaurantItem/index.js
@@ -24,12 +24,12 @@ const RestaurantItem = ({ restaurant }) => {
         <View>
           <Text style={styles.title}>{restaurant.name}</Text>
           <Text style={styles.subtitle}>
-            $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
-            {restaurant.maxDeliveryTime} minutes
+            $ {restaurant.deliveryFee.toFixed(2)} &#8226;{" "}
+            {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes
           </Text>
         </View>
         <View style={styles.rating}>
-          <Text>{restaurant.rating}</Text>
+          <Text>{restaurant.rating.toFixed(1)}</Text>
         </View>
       </View>
     </Pressable>
