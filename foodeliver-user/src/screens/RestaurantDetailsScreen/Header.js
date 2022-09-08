f28c378465ea8305bf8b86e4f1b7805dc7843940
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-08 19:56:01 +0300

    refactor - add float limit and image placeholder

--- foodeliver-user/src/screens/RestaurantDetailsScreen/Header.js
+++ foodeliver-user/src/screens/RestaurantDetailsScreen/Header.js
@@ -1,19 +1,24 @@
 import { View, Text, Image } from "react-native";
 import styles from "./styles";
+const DEFAULT_IMG = "https://fakeimg.pl/640x360";
 
 const RestaurantHeader = ({ restaurant }) => {
   return (
     <View style={styles.page}>
       <Image
-        source={{ uri: restaurant.image }}
+        source={{
+          uri: restaurant.image.startsWith("http")
+            ? restaurant.image
+            : DEFAULT_IMG,
+        }}
         style={styles.image}
         resizeMode="cover"
       />
       <View style={styles.container}>
         <Text style={styles.title}>{restaurant.name}</Text>
         <Text style={styles.subtitle}>
-          $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
-          {restaurant.maxDeliveryTime} minutes
+          $ {restaurant.deliveryFee.toFixed(2)} &#8226;{" "}
+          {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes
         </Text>
 
         <Text style={styles.menuTitle}>Menu</Text>
--- foodeliver-user/src/screens/RestaurantDetailsScreen/index.js
+++ foodeliver-user/src/screens/RestaurantDetailsScreen/index.js
@@ -1,19 +1,30 @@
-import { View, FlatList } from "react-native";
+import { View, FlatList, ActivityIndicator } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import restaurants from "../../../assets/data/restaurants.json";
 import DishListItem from "../../components/DishListItem";
 import Header from "./Header";
 import styles from "./styles";
 import { useRoute, useNavigation } from "@react-navigation/native";
-
-const restaurant = restaurants[0];
+import { useState, useEffect } from "react";
+import { DataStore } from "aws-amplify";
+import { Restaurant } from "../../models";
 
 const RestaurantDetailsPage = () => {
+  const [restaurant, setRestaurant] = useState(null);
+
   const route = useRoute();
   const navigation = useNavigation();
 
   const id = route.params?.id;
   // console.warn(id);
+
+  useEffect(() => {
+    // fetch restaurant with id
+    DataStore.query(Restaurant, id).then(setRestaurant);
+  }, []);
+
+  if (!restaurant) {
+    return <ActivityIndicator size={"large"} color={"darkgrey"} />;
+  }
 
   return (
     <View style={styles.page}>
