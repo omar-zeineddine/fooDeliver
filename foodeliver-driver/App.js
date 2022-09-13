7b133cfef65b90f3a6d3a85a24af2e67037bd5ab
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 08:29:54 +0300

    test - render first order

--- foodeliver-driver/App.js
+++ foodeliver-driver/App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import orders from "./assets/data/orders.json";
+
+const order = orders[0];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js</Text>
+      <View>
+        <Image
+          source={{ uri: order.Restaurant.image }}
+          style={{ width: 50, height: 50 }}
+        />
+        <Text>{order.Restaurant.name}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
