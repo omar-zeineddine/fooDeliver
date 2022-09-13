d9b5a849a72df936864b8fffd8521912805b92cb
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 11:18:12 +0300

    styles - add component styles in separate file

--- foodeliver-driver/src/screens/OrderDelivery/index.js
+++ foodeliver-driver/src/screens/OrderDelivery/index.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import { FontAwesome5, Fontisto } from "@expo/vector-icons";
 import orders from "../../../assets/data/orders.json";
+import styles from "./styles";
 
 const order = orders[0];
 
@@ -11,120 +12,42 @@ const OrderDelivery = () => {
 
   const snapPoints = useMemo(() => ["12%", "95%"], []);
   return (
-    <View style={{ backgroundColor: "lightblue", flex: 1 }}>
+    <View style={styles.container}>
       <BottomSheet
         ref={bottomSheetRef}
         snapPoints={snapPoints}
-        handleIndicatorStyle={{ backgroundColor: "grey", width: 100 }}
+        handleIndicatorStyle={styles.handleIndicator}
       >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            marginBottom: 20,
-          }}
-        >
-          <Text style={{ marginTop: 10, fontSize: 25, letterSpacing: 1 }}>
-            14 min
-          </Text>
+        <View style={styles.handleIndicatorContainer}>
+          <Text style={styles.routeDetailsText}>14 min</Text>
           <FontAwesome5
             name="shopping-bag"
             size={30}
             color="#3FC060"
             style={{ marginHorizontal: 10 }}
           />
-          <Text style={{ fontSize: 25, letterSpacing: 1 }}>5 Km</Text>
+          <Text style={styles.routeDetailsText}>5 Km</Text>
         </View>
-        <View style={{ paddingHorizontal: 20 }}>
-          <Text style={{ fontSize: 25, letterSpacing: 1, paddingVertical: 20 }}>
-            {order.Restaurant.name}
-          </Text>
-          <View
-            style={{
-              flexDirection: "row",
-              marginBottom: 20,
-              alignItems: "center",
-            }}
-          >
+        <View style={styles.deliveryDetailsContainer}>
+          <Text style={styles.restaurantName}>{order.Restaurant.name}</Text>
+          <View style={styles.addressContainer}>
             <Fontisto name="shopping-store" size={22} color="grey" />
-            <Text
-              style={{
-                fontSize: 20,
-                color: "grey",
-                fontWeight: "500",
-                letterSpacing: 0.5,
-                marginLeft: 15,
-              }}
-            >
-              {order.Restaurant.address}
-            </Text>
+            <Text style={styles.addressText}>{order.Restaurant.address}</Text>
           </View>
 
-          <View
-            style={{
-              flexDirection: "row",
-              marginBottom: 20,
-              alignItems: "center",
-            }}
-          >
+          <View style={styles.addressContainer}>
             <FontAwesome5 name="map-marker-alt" size={30} color="grey" />
-            <Text
-              style={{
-                fontSize: 20,
-                color: "grey",
-                fontWeight: "500",
-                letterSpacing: 0.5,
-                marginLeft: 15,
-              }}
-            >
-              {order.User.address}
-            </Text>
+            <Text style={styles.addressText}>{order.User.address}</Text>
           </View>
 
-          <View
-            style={{
-              borderTopWidth: 1,
-              borderColor: "lightgrey",
-              paddingTop: 20,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 18,
-                color: "grey",
-                fontWeight: "500",
-                letterSpacing: 0.5,
-                marginBottom: 5,
-              }}
-            >
-              Meal 1 x3
-            </Text>
-            <Text>Meal 2 x1</Text>
-            <Text>Meal 3 x2</Text>
+          <View style={styles.orderDetailsContainer}>
+            <Text style={styles.orderItemText}>Meal 1 x3</Text>
+            <Text style={styles.orderItemText}>Meal 2 x1</Text>
+            <Text style={styles.orderItemText}>Meal 3 x2</Text>
           </View>
         </View>
-        <View
-          style={{
-            backgroundColor: "#3FC060",
-            marginTop: "auto",
-            marginVertical: 30,
-            marginHorizontal: 10,
-            borderRadius: 10,
-          }}
-        >
-          <Text
-            style={{
-              color: "white",
-              textAlign: "center",
-              paddingVertical: 15,
-              fontSize: 25,
-              fontWeight: "500",
-              letterSpacing: 0.5,
-            }}
-          >
-            Accept order
-          </Text>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Accept order</Text>
         </View>
       </BottomSheet>
     </View>
