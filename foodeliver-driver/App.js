541517805bf02d7c488dc1b8e39427973b79cdf7
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 18:33:48 +0300

    fix - disable analytics module

--- foodeliver-driver/App.js
+++ foodeliver-driver/App.js
@@ -6,7 +6,7 @@ import Navigation from "./src/navigation";
 import { Amplify } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
 
-Amplify.configure(awsconfig);
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 export default function App() {
   return (
