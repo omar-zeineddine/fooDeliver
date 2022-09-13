b3946c61e9bfd0814d4940324088ecf9893e428c
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 09:55:37 +0300

    feat - render FlatList in orders screen sheet

--- foodeliver-driver/src/screens/OrdersScreen/index.js
+++ foodeliver-driver/src/screens/OrdersScreen/index.js
@@ -1,14 +1,15 @@
 import { useRef } from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import orders from "../../../assets/data/orders.json";
+import OrderItem from "../../components/OrderItem";
 
 const OrdersScreen = () => {
   const bottomSheetRef = useRef(null);
   return (
     <View style={{ backgroundColor: "lightblue", flex: 1 }}>
       <BottomSheet ref={bottomSheetRef} snapPoints={["12%", "95%"]}>
-        <View style={{ flex: 1, alignItems: "center" }}>
+        <View style={{ alignItems: "center", marginBottom: 30 }}>
           <Text
             style={{
               fontSize: 20,
@@ -23,6 +24,11 @@ const OrdersScreen = () => {
             Available Orders:{" "}
           </Text>
         </View>
+
+        <FlatList
+          data={orders}
+          renderItem={({ item }) => <OrderItem order={item} />}
+        />
       </BottomSheet>
     </View>
   );
