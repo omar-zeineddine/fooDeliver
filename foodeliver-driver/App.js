f1cd91396b29cd7da27f443f3b9078a97b2d6d63
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 18:31:06 +0300

    feat - import aws and amplify configs to app

--- foodeliver-driver/App.js
+++ foodeliver-driver/App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, FlatList } from "react-native";
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
 
-import orders from "./assets/data/orders.json";
-import OrdersScreen from "./src/screens/OrdersScreen";
-import OrderDelivery from "./src/screens/OrderDelivery";
-
-const order = orders[0];
+Amplify.configure(awsconfig);
 
 export default function App() {
   return (
@@ -17,8 +14,6 @@ export default function App() {
       <GestureHandlerRootView style={{ flex: 1 }}>
         <Navigation />
         <StatusBar style="auto" />
-        {/* <OrdersScreen /> */}
-        {/* <OrderDelivery /> */}
       </GestureHandlerRootView>
     </NavigationContainer>
   );
