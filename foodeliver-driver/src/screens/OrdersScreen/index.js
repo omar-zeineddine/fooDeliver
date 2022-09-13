1b67c6646d05844a6aa2949d5f4bb0542fe3b176
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 09:51:41 +0300

    style - add styles to order screen

--- foodeliver-driver/src/screens/OrdersScreen/index.js
+++ foodeliver-driver/src/screens/OrdersScreen/index.js
@@ -1,14 +1,27 @@
 import { useRef } from "react";
 import { View, Text } from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
+import orders from "../../../assets/data/orders.json";
 
 const OrdersScreen = () => {
   const bottomSheetRef = useRef(null);
   return (
     <View style={{ backgroundColor: "lightblue", flex: 1 }}>
       <BottomSheet ref={bottomSheetRef} snapPoints={["12%", "95%"]}>
-        <View style={{ flex: 1 }}>
-          <Text>Hello world</Text>
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "600",
+              letterSpacing: 0.6,
+              paddingBottom: 5,
+            }}
+          >
+            You're online{" "}
+          </Text>
+          <Text style={{ letterSpacing: 0.5, color: "grey" }}>
+            Available Orders:{" "}
+          </Text>
         </View>
       </BottomSheet>
     </View>
