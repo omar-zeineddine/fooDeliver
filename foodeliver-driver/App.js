278cc995c36a457cd8cb0c7034e93e81aa0b25b2
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 08:37:59 +0300

    style - update component style

--- foodeliver-driver/App.js
+++ foodeliver-driver/App.js
@@ -1,25 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import orders from "./assets/data/orders.json";
+import { Entypo } from "@expo/vector-icons";
 
 const order = orders[0];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={{ flexDirection: "row" }}>
         {/* restaurant */}
         <Image
           source={{ uri: order.Restaurant.image }}
           style={{ width: 50, height: 50 }}
         />
-        <Text>{order.Restaurant.name}</Text>
-        <Text>{order.Restaurant.address}</Text>
-        <Text>Delivery Details:</Text>
+        <View>
+          <Text>{order.Restaurant.name}</Text>
+          <Text>{order.Restaurant.address}</Text>
+          <Text>Delivery Details:</Text>
 
-        {/* user */}
-        <Text>{order.User.name}</Text>
-        <Text>{order.User.address}</Text>
+          {/* user */}
+          <Text>{order.User.name}</Text>
+          <Text>{order.User.address}</Text>
+        </View>
+        <Entypo name="check" size={30} color="black" />
       </View>
       <StatusBar style="auto" />
     </View>
