adf4393da2b152aa8e73b854e675c405b115887a
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 09:19:51 +0300

    feat - separate order item

--- foodeliver-driver/App.js
+++ foodeliver-driver/App.js
@@ -1,38 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
 import orders from "./assets/data/orders.json";
-import { Entypo } from "@expo/vector-icons";
-
-const order = orders[0];
+import OrderItem from "./src/components/OrderItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flexDirection: "row",
-          margin: 10,
-          borderColor: "#3FC060",
-          borderWidth: 2,
-          borderRadius: 10,
-        }}
-      >
-        {/* restaurant */}
-        <Image
-          source={{ uri: order.Restaurant.image }}
-          style={{ width: "25%", height: "100%" }}
-        />
-        <View>
-          <Text>{order.Restaurant.name}</Text>
-          <Text>{order.Restaurant.address}</Text>
-          <Text>Delivery Details:</Text>
-
-          {/* user */}
-          <Text>{order.User.name}</Text>
-          <Text>{order.User.address}</Text>
-        </View>
-        <Entypo name="check" size={30} color="black" />
-      </View>
+      <OrderItem />
       <StatusBar style="auto" />
     </View>
   );
