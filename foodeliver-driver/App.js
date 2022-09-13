4b60fa20fce2dab91248d5162f514252fabe6769
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-09-13 11:56:30 +0300

    feat - add navigation index

--- foodeliver-driver/App.js
+++ foodeliver-driver/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, FlatList } from "react-native";
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 
 import orders from "./assets/data/orders.json";
 import OrdersScreen from "./src/screens/OrdersScreen";
@@ -11,13 +12,15 @@ const order = orders[0];
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        {/* <OrdersScreen /> */}
-        <OrderDelivery />
-        <StatusBar style="auto" />
-      </View>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          {/* <OrdersScreen /> */}
+          <OrderDelivery />
+          <StatusBar style="auto" />
+        </View>
+      </GestureHandlerRootView>
+    </NavigationContainer>
   );
 }
 
